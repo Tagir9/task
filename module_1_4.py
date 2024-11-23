d7b1978e88cb845a0f927e24a122713a8564074d
Tagir9 <tagir886@gmail.com>
2024-11-23 12:06:01 +0300

    Add files via upload

---
+++ module_1_4.py
@@ -0,0 +1,14 @@
+# "Организация программ и методы строк"
+my_string=input("ввод строки: ")
+print("количество символов:",len(my_string))
+print(my_string.upper())
+print(my_string.lower())
+print(my_string.replace(" ",""))
+print(my_string[0])
+print(my_string[len(my_string)-1])#end
+
+
+
+
+
+
